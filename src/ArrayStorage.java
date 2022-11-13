332edb5b66625b2f9b79c6efb8e85a56fef9fde9
Maksim Lapushkin <ottlom@mail.ru>
2022-11-13 19:03:28 +0300

    corect remarks in ArrayStorage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -6,29 +6,24 @@ import java.util.stream.IntStream;
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
-    private int size = 0;
+    private int size;
+
     void clear() {
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] != null) {
-                storage[i] = null;
-            } else {
-                break;
-            }
-        }
+        Arrays.stream(storage, 0, size)
+                .forEach(resume -> resume = null);
         size = 0;
     }
 
     void save(Resume r) {
-        int lastIndex = size;
-        int sameResume = IntStream.range(0, lastIndex)
-                .filter(resume -> r.uuid.equals(storage[resume].uuid))
-                .findFirst()
-                .orElse(-1);
-        if (lastIndex > storage.length) {
+        if (size > storage.length) {
             System.out.println("В массиве закончилось место");
         } else {
+            int sameResume = IntStream.range(0, size)
+                    .filter(resume -> r.uuid.equals(storage[resume].uuid))
+                    .findFirst()
+                    .orElse(-1);
             if (sameResume == -1) {
-                storage[lastIndex] = r;
+                storage[size] = r;
                 System.out.println("resume " + r.uuid + " was added");
                 size++;
             } else {
@@ -38,42 +33,31 @@ public class ArrayStorage {
     }
 
     Resume get(String uuid) {
-        try {
-            Resume returnResume = Arrays.stream(storage)
-                    .filter(resume -> resume.uuid.equals(uuid))
-                    .findFirst()
-                    .get();
-            return returnResume;
-        } catch (NullPointerException e) {
-            System.out.println("such resume with uuid = " + uuid + " not found in list");
-        }
-        return null;
+        Resume returnResume = Arrays.stream(storage, 0, size)
+                .filter(resume -> resume.uuid.equals(uuid))
+                .findFirst()
+                .orElse(null);
+        return returnResume;
     }
 
     void delete(String uuid) {
-        try {
-            int index = IntStream.range(0, storage.length)
-                    .filter(resume -> storage[resume].uuid.equals(uuid))
-                    .findFirst()
-                    .orElse(-1);
-            Resume[] copy = Arrays.copyOfRange(storage, index + 1, storage.length);
-            System.arraycopy(copy, 0, storage, index, copy.length);
+        int index = IntStream.range(0, size)
+                .filter(resume -> storage[resume].uuid.equals(uuid))
+                .findFirst()
+                .orElse(-1);
+        if (index == -1) {
+            System.out.println("You try delete element such not found in list");
+        } else {
+            System.arraycopy(storage, index + 1, storage, index, size);
             size--;
-        } catch (NullPointerException e) {
-            System.out.println("such resume with uuid = " + uuid + " not found in list");
         }
-
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        int index = IntStream.range(0, storage.length)
-                .filter(resume -> storage[resume] == null)
-                .findFirst()
-                .orElse(-1);
-        return Arrays.copyOfRange(storage, 0, index);
+        return Arrays.copyOfRange(storage, 0, size);
     }
 
     int size() {

e50dfaf829d0a57808d86498ac4271b4203e75bc
Maksim Lapushkin <ottlom@mail.ru>
2022-11-25 19:25:59 +0300

    make method update and create packages

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -1,3 +1,6 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.model.Resume;
 import java.util.Arrays;
 import java.util.stream.IntStream;
 
@@ -5,56 +8,66 @@ import java.util.stream.IntStream;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    private Resume[] storage = new Resume[10000];
     private int size;
 
-    void clear() {
+    public void clear() {
         Arrays.stream(storage, 0, size)
                 .forEach(resume -> resume = null);
         size = 0;
     }
 
-    void save(Resume r) {
+    public void save(Resume r) {
         if (size == storage.length) {
             System.out.println("Array ran out of space");
-        } else if (getIndex(r.uuid) == -1) {
+        } else if (getIndex(r.getUuid()) == -1) {
             storage[size] = r;
-            System.out.println("resume " + r.uuid + " was added");
+            System.out.println("resume " + r.getUuid() + " was added");
             size++;
         } else {
-            System.out.println("List's already have resume with uuid = " + r.uuid);
+            System.out.println("List's already have resume with uuid = " + r.getUuid());
         }
     }
 
-    Resume get(String uuid) {
+    public Resume get(String uuid) {
         int index = getIndex(uuid);
         return index != -1 ? storage[index] : null;
     }
 
-    void delete(String uuid) {
+    public void delete(String uuid) {
         int index = getIndex(uuid);
         if (index == -1) {
             System.out.println("You try delete element such not found in list");
         } else {
-            System.arraycopy(storage, index + 1, storage, index, size - index);
+            System.arraycopy(storage, index + 1, storage, index, size - index - 1);
             size--;
+        }
+    }
+
+    public void update(Resume resume) {
+        int index = getIndex(resume.getUuid());
+        if (index == -1) {
+            System.out.println("You try update nonexistent resume");
+        } else {
+            storage[index] = resume;
+            System.out.println("resume " + resume.getUuid() + " was update");
         }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
+    public Resume[] getAll() {
         return Arrays.copyOf(storage, size);
     }
 
-    int size() {
+    public int size() {
         return size;
     }
 
     private int getIndex(String uuid) {
         return IntStream.range(0, size)
-                .filter(resume -> storage[resume].uuid.equals(uuid))
+                .filter(resume -> storage[resume].getUuid().equals(uuid))
                 .findFirst()
                 .orElse(-1);
     }
--- src/com/urise/webapp/storage/MainTestArrayStorage.java
+++ src/com/urise/webapp/storage/MainTestArrayStorage.java
@@ -1,5 +1,9 @@
+package com.urise.webapp.storage;
+
+import com.urise.webapp.model.Resume;
+
 /**
- * Test for your ArrayStorage implementation
+ * Test for your com.urise.webapp.storage.ArrayStorage implementation
  */
 public class MainTestArrayStorage {
     static final ArrayStorage ARRAY_STORAGE = new ArrayStorage();
@@ -7,28 +11,33 @@ public class MainTestArrayStorage {
     public static void main(String[] args) {
 
         Resume r1 = new Resume();
-        r1.uuid = "uuid1";
+        r1.setUuid("uuid1");
         Resume r2 = new Resume();
-        r2.uuid = "uuid2";
+        r2.setUuid("uuid2");
         Resume r3 = new Resume();
-        r3.uuid = "uuid3";
+        r3.setUuid("uuid3");
 
         ARRAY_STORAGE.save(r1);
         ARRAY_STORAGE.save(r2);
         ARRAY_STORAGE.save(r3);
 
-        System.out.println("Get r1: " + ARRAY_STORAGE.get(r1.uuid));
+        System.out.println("Get r1: " + ARRAY_STORAGE.get(r1.getUuid()));
         System.out.println("Size: " + ARRAY_STORAGE.size());
 
         System.out.println("Get dummy: " + ARRAY_STORAGE.get("dummy"));
 
         printAll();
-        ARRAY_STORAGE.delete(r1.uuid);
+        ARRAY_STORAGE.delete(r1.getUuid());
         printAll();
         ARRAY_STORAGE.clear();
         printAll();
 
         System.out.println("Size: " + ARRAY_STORAGE.size());
+        ARRAY_STORAGE.save(r1);
+        System.out.println("Get r1: " + ARRAY_STORAGE.get(r1.getUuid()));
+        r1.setUuid("uuid1Updated");
+        ARRAY_STORAGE.update(r1);
+        System.out.println("Get r1: " + ARRAY_STORAGE.get(r1.getUuid()));
     }
 
     static void printAll() {

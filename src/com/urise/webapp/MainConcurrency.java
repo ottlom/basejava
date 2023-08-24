d08ab22f1a05f67e45ad8d0ef403a55f62ed227f
Maksim Lapushkin <ottlom@mail.ru>
2023-08-24 17:59:44 +0300

    hw_12 practice with streamAPI

--- src/com/urise/webapp/MainConcurrency.java
+++ src/com/urise/webapp/MainConcurrency.java
@@ -1,5 +1,10 @@
 package com.urise.webapp;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class MainConcurrency {
     private static final Object LOCK1 = new Object();
     private static final Object LOCK2 = new Object();
@@ -13,11 +18,18 @@ public class MainConcurrency {
             doSynchronize(LOCK2, LOCK1);
         });
 
-        thread1.start();
-        thread2.start();
+        //thread1.start();
+        //thread2.start();
 
         thread1.join();
         thread2.join();
+
+        minValue(new int[]{1, 9, 2, 7, 8, 5, 3, 4, 9, 5, 2, 1, 3});
+
+        List<Integer> list = new ArrayList<>(Arrays.asList(1,4,3,5));
+        System.out.println(list);
+        oddOrEven(list);
+        System.out.println(list);
     }
 
     public static void doSynchronize(Object lock1, Object lock2) {
@@ -31,5 +43,23 @@ public class MainConcurrency {
                 System.out.println("some work from " + Thread.currentThread().getName());
             }
         }
+    }
+
+    public static int minValue(int[] values) {
+        int resultNum = Arrays.stream(values)
+                .distinct()
+                .sorted()
+                .reduce(0, (result, num) -> result * 10 + num);
+        System.out.println(resultNum);
+        return resultNum;
+    }
+
+    public static List<Integer> oddOrEven(List<Integer> integers) {
+        integers.removeAll(integers.stream()
+                .collect(Collectors.collectingAndThen(
+                        Collectors.partitioningBy(i -> i % 2 == 0),
+                        map -> map.get(integers.stream().mapToInt(Integer::intValue).sum() % 2 == 0)
+                )));
+        return integers;
     }
 }
